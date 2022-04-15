d263d9fe2ffaf96fa4f0afaeec160563641d9aae
wnyao <wenyao_k@hotmail.com>
2022-04-15 23:02:02 +0800

    fix: Replace github link

--- packages/client/src/components/Layout/configs.ts
+++ packages/client/src/components/Layout/configs.ts
@@ -6,6 +6,6 @@ interface IBottomTabs {
 export const bottomTabs: IBottomTabs[] = [
   {
     name: () => i18n.t('GitHub'),
-    href: 'https://github.com/wnyao'
+    href: 'https://github.com/wnyao/scaffold-lerna'
   }
 ];
