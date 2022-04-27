f91e9f7e22be7ac71e355d99afe63d2620f8b4d1
wnyao <wenyao_k@hotmail.com>
2022-04-27 18:02:37 +0800

    fix: Resolve ts-lint warning on third party dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
+const tsConfig = require('./packages/core/tsconfig.json');
+
 module.exports = {
   extends: 'wnyao',
   rules: {
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-var-requires': 'off'
-  }
+  },
+  ignorePatterns: tsConfig.exclude
 };
